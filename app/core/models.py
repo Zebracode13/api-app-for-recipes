355b54048f863bf496fb1f724b291e2690b221ea
Zele_13 <zele_selam@Zele-13s-Air.attlocal.net>
2020-10-23 15:40:08 -0700

    test backend data

--- app/core/models.py
+++ app/core/models.py
@@ -1,19 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
-                                        PermissionMixin)
+                                        PermissionsMixin)
 
 # Create your models here.
 class UserManger(BaseUserManager):
-
     def create_user(self, email, password=None, **extra_feilds):
         """Creates and saves a new user"""
-        user = self.model(email=email, **extra_feilds)
+        user = self.model(email=self.normalize_email(email), **extra_feilds)
         user.set_password(password)
         user.save(using=self.db)
 
         return user
 
-class User(AbstractBaseUser, PermissionMixin):
+class User(AbstractBaseUser, PermissionsMixin):
     """costum user modle that supporst wmail instead of username"""
     email = models.EmailField(max_length=264, unique=True)
     name = models.CharField(max_length=264)
@@ -22,4 +21,3 @@ class User(AbstractBaseUser, PermissionMixin):
 
     objects = UserManger()
     USERNAME_FIELD = 'email'
-
