68c4de109ea445e61d9525dbd5b300814deb9535
Zele_13 <zele_selam@Zele-13s-MacBook-Air.local>
2020-10-23 11:01:00 -0700

    added test unites

---
+++ app/core/test/test_models.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTests(TestCase):
+    
+    def test_create_user_email(self):
+        """Test of email for user creation is working propperly"""
+        email = 'ze@gmail.com'
+        password = 'test123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user, email, email)
+        self.assertTrue(user.check_password(password))
+        
